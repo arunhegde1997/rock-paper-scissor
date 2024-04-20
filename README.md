b588d10da84947715edc8da46c18ef75a0e94cc5
Gourisha Hegde <gourishdhegde@gmail.com>
2024-04-20 12:05:51 +0530

    Learning new branch creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# rock-paper-scissor" 
+
+This is my first personal project using HTML5, CSS3 and JavaScript.
--- sampleDummyFile.js
+++
@@ -1,6 +0,0 @@
-// My dummy file
-
-// My dummy file - line 2
-// My dummy file - line 3
-// My dummy file - line 4
-// My dummy file - line 5
